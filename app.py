83a6cf945584eed65c662f4691fc5a429927edee
Sarah Szuminski <smszuminski@hotmail.com>
2013-03-02 18:01:58 -0600

    debugging

--- app.py
+++ app.py
@@ -9,21 +9,23 @@ Flask and sqlite3.
 :copyright: (c) 2010 by Armin Ronacher.
 :license: BSD, see LICENSE for more details.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 SECRET_KEY = 'development key'
 
 # create our little application :)
 app = Flask(__name__)
 app.debug = True
+
+@app.route('/video/<videoid>')
+def showvideo(videoid="unset", methods=["GET"]):
+	return render_template("video.html", videoid=videoid)
+ #   return output
+
 @app.route('/')
 def main_site():
 	return render_template("index.html")
  #   return output
 
-@app.route('/video/<videoid>')
-def show_video(videoid="unset"):
-	return render_template("video.html", videoid=videoid)
- #   return output
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
